f5949ff185201bb8c481ee8a5573986a468d54b9
Rohit Goswami <rog32@hi.is>
2023-08-30 20:21:17 +0000

    MAINT: Appease linter

--- R/fastMatMR-package.R
+++ R/fastMatMR-package.R
@@ -8,40 +8,46 @@ NULL
 
 #' Convert Various Numeric Types to Matrix Market Format
 #'
-#' This function takes different types of numeric inputs—vectors, matrices, and sparse matrices—
-#' and converts them into Matrix Market files. The output file is written to disk.
+#' This function takes different types of numeric inputs—vectors, matrices, and
+#' sparse matrices— and converts them into Matrix Market files. The output file
+#' is written to disk.
 #'
-#' @param input A numeric object to be converted. This can be a numeric vector, a matrix, or a sparse matrix.
-#' @param fname The name of the output file where the Matrix Market formatted data will be saved.
-#'             It is recommended to use a filename ending with ".mtx" for clarity.
+#' @param input A numeric object to be converted. This can be a numeric vector,
+#'   a matrix, or a sparse matrix.  @param fname The name of the output file
+#'   where the Matrix Market formatted data will be saved.  It is recommended to
+#'   use a filename ending with ".mtx" for clarity.
 #'
-#' @return This function has no return value. It writes a Matrix Market formatted file to disk.
+#' @return This function has no return value. It writes a Matrix Market
+#'   formatted file to disk.
 #'
 #' @examples
 #' \dontrun{
 #' vec <- c(1, 2, 3)
 #' mat <- matrix(c(1, 2, 3, 4), nrow = 2)
 #' sparse_mat <- Matrix::Matrix(c(1, 0, 0, 2), nrow = 2, sparse = TRUE)
-#' Diagonal ^-
+#' ## Diagonal ^-
 #' sparse_mat <- Matrix::Matrix(c(1, 1, 0, 2), nrow = 2, sparse = TRUE)
-#' And not diagonal -^
+#' ## And not diagonal -^
 #'
-#' writeFMM(vec, "vector.mtx")
-#' writeFMM(mat, "matrix.mtx")
-#' writeFMM(sparse_mat, "sparse_matrix.mtx")
+#' write_fmm(vec, "vector.mtx")
+#' write_fmm(mat, "matrix.mtx")
+#' write_fmm(sparse_mat, "sparse_matrix.mtx")
 #' }
 #'
 #' @export
-writeFMM <- function(input, fname = "out.mtx") {
+write_fmm <- function(input, fname = "out.mtx") {
   if (is.vector(input)) {
-    return(vec_to_fmm(input, fname))
+    return(vec_to_fmm(input, fname)) # nolint. C++ function.
   } else if (is.matrix(input)) {
-    return(mat_to_fmm(input, fname))
-  }
-  else if (inherits(input, "sparseMatrix")) {
-    return(sparse_to_fmm(input, fname))
-  }
-  else {
-    stop("Unsupported input type. Accepted types are numeric vector, matrix, and sparseMatrix.")
+    return(mat_to_fmm(input, fname)) # nolint. C++ function.
+  } else if (inherits(input, "sparseMatrix")) {
+    return(sparse_to_fmm(input, fname)) # nolint. C++ function.
+  } else {
+    stop(
+      paste(
+        "Unsupported input type.",
+        "Accepted types are numeric vector, matrix, and sparseMatrix."
+      )
+    )
   }
 }
--- src/to_file.cpp
+++ src/to_file.cpp
@@ -1,102 +1,100 @@
 #include <cstdint>
 #include <cstdlib>
-#include<vector>
-#include<iostream>
-#include<cstring>
-#include<filesystem>
-#include<fstream>
+#include <cstring>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <vector>
 
 #include "cpp11.hpp"
 
 #include "../inst/include/fast_matrix_market/fast_matrix_market.hpp"
 
 namespace fmm = fast_matrix_market;
-using namespace std::string_literals;
 
-[[cpp11::register]]
+[[cpp11::register]] //
 int vec_to_fmm(cpp11::doubles r_vec, std::string filename) {
-    std::string mm;
-    std::vector<double> std_vec(r_vec.size());
-    std::copy(r_vec.begin(), r_vec.end(), std_vec.begin());
-    fmm::matrix_market_header header(1, std_vec.size());
-    // header.comment = std::string("comment");
-    // Use C++17 filesystem to construct ofstream
-    std::filesystem::path file_path(filename);
-    std::ofstream os(file_path);
+  std::string mm;
+  std::vector<double> std_vec(r_vec.size());
+  std::copy(r_vec.begin(), r_vec.end(), std_vec.begin());
+  fmm::matrix_market_header header(1, std_vec.size());
+  // header.comment = std::string("comment");
+  // Use C++17 filesystem to construct ofstream
+  std::filesystem::path file_path(filename);
+  std::ofstream os(file_path);
 
-    if (!os.is_open()) {
-        // Handle error
-        return EXIT_FAILURE;
-    }
-    fmm::write_matrix_market_array(os, header, std_vec);
-    os.close();
-    return EXIT_SUCCESS;
+  if (!os.is_open()) {
+    // Handle error
+    return EXIT_FAILURE;
+  }
+  fmm::write_matrix_market_array(os, header, std_vec);
+  os.close();
+  return EXIT_SUCCESS;
 }
 
-[[cpp11::register]]
-int mat_to_fmm(cpp11::doubles_matrix<> r_mat, std::string filename) {
-    // Get matrix dimensions
-    int nrows = r_mat.nrow();
-    int ncols = r_mat.ncol();
+[[cpp11::register]] //
+int mat_to_fmm(cpp11::doubles_matrix<> r_mat,
+               std::string filename) {
+  // Get matrix dimensions
+  int nrows = r_mat.nrow();
+  int ncols = r_mat.ncol();
 
-    std::vector<double> std_vec(nrows * ncols);
-    int k = 0;
-    for(int i = 0; i < nrows; ++i) {
-        for(int j = 0; j < ncols; ++j) {
-            std_vec[k++] = r_mat(i, j);
-        }
+  std::vector<double> std_vec(nrows * ncols);
+  int k = 0;
+  for (int i = 0; i < nrows; ++i) {
+    for (int j = 0; j < ncols; ++j) {
+      std_vec[k++] = r_mat(i, j);
     }
+  }
 
-    fmm::matrix_market_header header(nrows, ncols);
+  fmm::matrix_market_header header(nrows, ncols);
 
-    std::filesystem::path file_path(filename);
-    std::ofstream os(file_path);
-    if (!os.is_open()) {
-        return EXIT_FAILURE;
-    }
+  std::filesystem::path file_path(filename);
+  std::ofstream os(file_path);
+  if (!os.is_open()) {
+    return EXIT_FAILURE;
+  }
 
-    fmm::write_matrix_market_array(os, header, std_vec);
-    os.close();
-    return EXIT_SUCCESS;
+  fmm::write_matrix_market_array(os, header, std_vec);
+  os.close();
+  return EXIT_SUCCESS;
 }
 
-[[cpp11::register]] int sparse_to_fmm(cpp11::sexp input, std::string filename) {
-    std::vector<int> i_vec, p_vec; // not set for diagonal sparse matrices
+[[cpp11::register]] //
+int sparse_to_fmm(cpp11::sexp input, std::string filename) {
+  std::vector<int> i_vec, p_vec; // not set for diagonal sparse matrices
+  std::vector<double> x_vec =
+      cpp11::as_cpp<std::vector<double>>(input.attr("x"));
+  std::vector<int> dim_vec = cpp11::as_cpp<std::vector<int>>(input.attr("Dim"));
+
+  fmm::matrix_market_header header(dim_vec[0], dim_vec[1]);
+
+  // Check if the matrix is a diagonal matrix by its class
+  std::string matrix_class =
+      cpp11::as_cpp<std::string>(cpp11::as_sexp(input.attr("class")));
+
+  if (matrix_class == "ddiMatrix") {
     std::vector<double> x_vec =
-        cpp11::as_cpp<std::vector<double>>(input.attr("x"));
-    std::vector<int> dim_vec =
-        cpp11::as_cpp<std::vector<int>>(input.attr("Dim"));
-
-    fmm::matrix_market_header header(dim_vec[0], dim_vec[1]);
-
-    // Check if the matrix is a diagonal matrix by its class
-    std::string matrix_class =
-        cpp11::as_cpp<std::string>(cpp11::as_sexp(input.attr("class")));
-
-    if (matrix_class == "ddiMatrix") {
-        std::vector<double> x_vec =
-            cpp11::as_cpp<std::vector<double>>(cpp11::as_sexp(input.attr("x")));
-        // Create i, p, x vectors for diagonal matrices
-        for (int j = 0; j < dim_vec[1]; ++j) {
-            p_vec.push_back(j);
-            i_vec.push_back(j);
-        }
-        p_vec.push_back(dim_vec[1]);
-    } else {
-        p_vec =
-            cpp11::as_cpp<std::vector<int>>(cpp11::as_sexp(input.attr("p")));
-        i_vec =
-            cpp11::as_cpp<std::vector<int>>(cpp11::as_sexp(input.attr("i")));
+        cpp11::as_cpp<std::vector<double>>(cpp11::as_sexp(input.attr("x")));
+    // Create i, p, x vectors for diagonal matrices
+    for (int j = 0; j < dim_vec[1]; ++j) {
+      p_vec.push_back(j);
+      i_vec.push_back(j);
     }
+    p_vec.push_back(dim_vec[1]);
+  } else {
+    p_vec = cpp11::as_cpp<std::vector<int>>(cpp11::as_sexp(input.attr("p")));
+    i_vec = cpp11::as_cpp<std::vector<int>>(cpp11::as_sexp(input.attr("i")));
+  }
 
-    std::ofstream os(filename);
-    if (!os.is_open()) {
-        return EXIT_FAILURE;
-    }
+  std::ofstream os(filename);
+  if (!os.is_open()) {
+    return EXIT_FAILURE;
+  }
 
-    fmm::write_matrix_market_csc(os, header, p_vec, i_vec, x_vec, false);
+  fmm::write_matrix_market_csc(os, header, p_vec, i_vec, x_vec, false);
 
-    os.close();
+  os.close();
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }

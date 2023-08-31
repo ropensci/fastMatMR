8a669f21b2bde52d68f9aec1d704b7ce8b5b34ce
Rohit Goswami <rog32@hi.is>
2023-08-31 05:32:58 +0000

    MAINT: Rework vignette caching script

--- rebuild-benchmarks.R
+++ rebuild-benchmarks.R
@@ -1,8 +1,16 @@
 ## Kanged and updated from https://www.kloppenborg.ca/2021/06/long-running-vignettes/
+
 devtools::clean_dll()
 cpp11::cpp_register()
 devtools::document()
 devtools::load_all()
 
-knitr::knit("vignettes/fmm_write_bench.Rmd.orig", output = "vignettes/fmm_write_bench.Rmd")
-knitr::purl("vignettes/fmm_write_bench.Rmd.orig", output = "vignettes/fmm_write_bench.R")
+
+old_wd <- getwd()
+
+setwd("vignettes/")
+
+knitr::knit("fmm_write_bench.Rmd.orig", output = "fmm_write_bench.Rmd")
+knitr::purl("fmm_write_bench.Rmd.orig", output = "fmm_write_bench.R")
+
+setwd(old_wd)

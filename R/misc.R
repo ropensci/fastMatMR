563af2a14230852fc601d0c23451653b1dadbb9f
Rohit Goswami <rog32@hi.is>
2023-09-02 10:39:04 +0000

    MAINT: Use line coverage ignores

--- R/misc.R
+++ R/misc.R
@@ -1,7 +1,7 @@
 # To add extra release questions that devtools::release asks
 # Ref. https://devtools.r-lib.org/reference/release.html
-release_questions <- function() {
+release_questions <- function() { # nocov start
   c(
     "Did you re-build the benchmark vignettes using `rebuild-benchmarks.R`?"
   )
-}
+} # nocov end
